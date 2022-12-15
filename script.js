9e4838a06833a808ae5798ac0bb5a3b1c84d23c0
Anish kumar Srinivaasan <anishkumar92@gmail.com>
2022-12-15 19:10:34 +0530

    twitter and newQuote functionality

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let apiQuotes = [];
+
+function newQuote() {
+  // pick a rondom quote from apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  authorText.textContent = quote.author ? quote.author : "Unknown";
+  quoteText.textContent = quote.text;
+}
+
 // Get Quotes from API
-async function getQuotes(){
-    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
-    try{
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
-    } catch(error){
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
     // catch error
     alert(error);
-    }
+  }
 }
+
+//  Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
 
 // onLoad
 
