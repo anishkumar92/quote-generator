55b884a5e5dd54c9d6d7bfb361216b59a8d53c2f
Anish kumar Srinivaasan <anishkumar92@gmail.com>
2022-12-15 20:00:37 +0530

    added loader

--- script.js
+++ script.js
@@ -3,18 +3,34 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// show showLoadingSpinner
+function showLoadingSpinner(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//  Hide Loder
+function removeLoadingSpinner(){
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+}
+
+
 function newQuote() {
-  // pick a rondom quote from apiQuotes array
+    showLoadingSpinner();// pick a rondom quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   authorText.textContent = quote.author ? quote.author : "Unknown";
   quoteText.textContent = quote.text;
+  removeLoadingSpinner();
 }
 
 // Get Quotes from API
 async function getQuotes() {
+    showLoadingSpinner();
   const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
   try {
     const response = await fetch(apiUrl);
